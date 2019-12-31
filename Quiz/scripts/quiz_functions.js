bd5a5764292b38ff255d81b992b48053f073923d
UltimateCutlery <57415618+UltimateCutlery@users.noreply.github.com>
2019-12-31 00:42:16 +0000

    fixed?

--- Quiz/scripts/quiz_functions.js
+++ Quiz/scripts/quiz_functions.js
@@ -1,44 +1,56 @@
 var score= 0;
 var counter = 0;
 
-function buildQuiz() {
+
+function buildQuiz() {   // this bit displays qs or results
+    document.querySelector(".question").innerHTML = Questions[counter];
+    document.getElementById('a').innerHTML = Questions[counter+1];
+    document.getElementById('b').innerHTML = Questions[counter+2];
+    document.getElementById('c').innerHTML = Questions[counter+3];
+    document.getElementById('d').innerHTML = Questions[counter+4];
+    correctvalue=Questions[counter+5];
+    console.log("counter =" + counter);
+    console.log("the correct answer is " + correctvalue);
+    
+
     if (counter < Questions.length-5) {
-        DisplayQs()
+        TakeAns()
     } else {
         showresults()
     }
 }
 
 
-function DisplayQs(){ // this bit displays the questions and registers when an answer is clicked. You only get one shot, a back button or something might be useful
-    document.querySelector(".question").innerHTML = Questions[counter];
-    document.getElementById('a').innerHTML = Questions[counter+1];
-    document.getElementById('b').innerHTML = Questions[counter+2];
-    document.getElementById('c').innerHTML = Questions[counter+3];
-    document.getElementById('d').innerHTML = Questions[counter+4];
-    var correctvalue=Questions[counter+5];
 
-    let buttons = document.getElementsByTagName("button");
+function TakeAns(){ // this bit registers when an answer is clicked. You only get one shot, a back button or something might be useful
+
+    var buttons = document.getElementsByTagName("button");
+    
         for ( let i = 0; i < buttons.length; i++){
             let button=buttons[i];
 
-    if(button.innerHTML==correctvalue) //if the right answer is pressed then the score gets increased and the next question loaded
-        button.addEventListener("click", function(){
-        score += 1;
-        counter += 6;
-        buildQuiz();
-        console.log( "score =" + score);
-    });
+            button.removeEventListener("click", clickbutton);
+            button.addEventListener("click", clickbutton);
 
-    else { //if any other answer is pressed then the next question loaded
-        button.addEventListener("click", function(){ 
-        counter += 6;
-        buildQuiz();
-        console.log("score =" + score);
-    });
-
+            if(button.innerHTML==correctvalue) {//if the right answer is pressed then the score gets increased 
+                button.removeEventListener("click", takescore);
+                button.addEventListener("click", takescore);
+                    
+        }
+            
     }
+    console.log( "score =" + score);    
 }
+
+function clickbutton(){ //when a button is pressed
+    counter += 6; //the next q is found
+    buildQuiz(); //the q is refreshed
+    console.log( "counter increased by 6");
+}
+
+function takescore(){ 
+    score += 1; 
+    console.log( "score increased by 1");
 }
 
 //this displays the score after the test - Could definitely be improved with a popup or something
